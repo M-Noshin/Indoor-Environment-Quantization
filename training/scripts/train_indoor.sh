474f15dcf1e16958387d2c7e5ed4653f0a2939d7
7abushahla <b00090279@aus.edu>
2025-08-25 01:36:04 +0400

    update training script

--- training/scripts/train_indoor.sh
+++ training/scripts/train_indoor.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 python train.py --epochs 10 --batch-size 256 \
-  --optimizer Adam --lr 0.001 --weight-decay 0.0005 \
+  --optimizer Adam --lr 0.001 --weight-decay 0 \
   --use-bias --deterministic \
-  --model ai85indoorenvnetv1 --dataset IndoorEnvironmentAug --data data/indoor_environment \
-  --compress policies/schedule-indoor-env.yaml \
-  --qat-policy policies/qat_policy_indoor.yaml \
-  --device MAX78002 --name indoor_run_8Bit "$@"
+  --model ai85indoorenvnetv1 --dataset IndoorEnvironment --data data/indoor_environment \
+  --compress policies/schedule-indoor-env-phase1-exact.yaml \
+  --qat-policy None \
+  --device MAX78002 --name indoor_run_no_qat "$@"
+  # --qat-policy policies/qat_policy_indoor.yaml \
